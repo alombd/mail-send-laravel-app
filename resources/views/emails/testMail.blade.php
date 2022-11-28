6c8ada65d457f570f4d742afabdd82265e831a87
alombd <alombd.bd@gmail.com>
2022-11-28 19:18:07 +0600

    Testing email sending

--- resources/views/emails/testMail.blade.php
+++ resources/views/emails/testMail.blade.php
@@ -2,8 +2,18 @@
 <html>
 <head>
     <title>Testing Email address</title>
+    <style>
+    .logo {
+        padding: 20px;
+        background: darkolivegreen;
+        text-align: center;
+    }
+    </style>
 </head>
 <body>
+    <div class="logo">
+        <img src="{{asset('images/logo.png')}}" alt="profile Pic" height="100" width="100">
+    </div>
     <h1>{{ $details['title'] }}</h1>
     <p>{{ $details['body'] }}</p>
    
